ee7eab33bd9f852777a6d6ad7b4533dc67076b21
David Talevski <david.talevski@x3mlabs.com>
2023-05-14 23:07:04 +0200

    Implemented Firebase Authentication

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -14,6 +15,10 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+
+import java.util.Arrays;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -50,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             setLocale("en");
         }
 
-        restartActivity();
+        recreate();
     }
 
     private void setLocale(String languageCode) {
@@ -62,9 +67,26 @@ public class MainActivity extends AppCompatActivity {
         res.updateConfiguration(config, res.getDisplayMetrics());
     }
 
-    public void restartActivity() {
-        Intent intent = getIntent();
-        finish();
-        startActivity(intent);
+    private ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            (result) -> {
+                Log.d("asd", result.toString());
+            });
+
+    public void startSignIn(View v) {
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.AnonymousBuilder().build()))
+                .build();
+
+        signInLauncher.launch(signInIntent);
+    }
+    public void onSignOutClicked(View v) {
+        AuthUI.getInstance()
+                .signOut(this);
     }
 }
